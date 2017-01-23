d44780e3663e1dbb6eb44f864b585125c2d5ca64
Stephen Igwue <olibenu@yahoo.com>
2017-01-23 14:09:55 +0100

    added support for composer installed third party libs

--- engine/iriki/session.php
+++ engine/iriki/session.php
@@ -6,7 +6,18 @@ class session
 {
 	public function initiate($params_persist = null)
 	{
-		
+        $instance = new $params_persist['db_type']();
+        $instance::initInstance();
+
+        //do validation of params (count check and isset?)
+        //if mode is strict and this check fails, do not call create
+
+        //add created and modified timestamps?
+
+        if (!is_null($params_persist))
+        {
+            return $instance::doCreate($params_persist);
+        }
 	}
 }
 
--- vendors/autoload.php
+++ vendors/autoload.php
@@ -3,4 +3,7 @@
 require_once(__DIR__ . '/naija_pikin/naija_pikin.php');
 require_once(__DIR__ . '/redbean/rb.php');
 
+//composer
+require_once(__DIR__ . '/vendor/autoload.php');
+
 ?>
