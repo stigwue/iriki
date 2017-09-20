e171b0d5922cd8020187b6531fe0850a0244e42e
Stephen Igwue <olibenu@yahoo.com>
2017-09-20 17:30:46 +0100

    added mongodb tests and travis config file

--- engine/tests/database/databaseTest.php
+++ engine/tests/database/databaseTest.php
@@ -4,7 +4,6 @@ class databaseTest extends \PHPUnit\Framework\TestCase
 {
     public function test_doInitialise_failure()
     {
-        $db = new \iriki\engine\database();
         $status = iriki\engine\database::doInitialise(null, 'iriki');
 
         //assert
---
+++ engine/tests/database/mongodbTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class mongodbTest extends \PHPUnit\Framework\TestCase
+{
+    public function test_doInitialise_success()
+    {
+        $status = iriki\engine\database::doInitialise(
+            'kronos',
+            'iriki',
+            array(
+                'type' => 'mongodb',
+                'server' => 'mongodb://localhost:27017',
+                'db' => 'kronos'
+            )
+        );
+
+        //assert
+        $this->assertEquals(true, $status);
+    }
+
+    public function test_isMongoId_failure()
+    {
+        $false_mongo_id = 'false mongo id';
+
+        $status = \iriki\engine\mongodb::isMongoId($false_mongo_id);
+
+        //assert
+        $this->assertNotEquals(true, $status);
+    }
+
+    public function test_isMongoId_success()
+    {
+        $true_mongo_id = '596cbd52565bb550080041b8';
+
+        $status = \iriki\engine\mongodb::isMongoId($true_mongo_id);
+
+        //assert
+        $this->assertEquals(true, $status);
+    }
+}
+
+?>
