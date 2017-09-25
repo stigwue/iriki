541c2cf6ab965f945431c09a57231b4bfe8c3815
Stephen Igwue <olibenu@yahoo.com>
2017-09-25 20:11:33 +0100

    wrappedup mongodb crud success tests and dependencies

--- engine/tests/database/mongodberrorTest.php
+++ engine/tests/database/mongodberrorTest.php
@@ -172,6 +172,166 @@ class mongodberrorTest extends \PHPUnit\Framework\TestCase
         //assert
         $this->assertEquals(true, isset($status['code']));
     }
+
+    public function test_doUpdate_error()
+    {
+        iriki\engine\mongodb::doInitialise(
+            array(
+                'type' => '\\iriki\\engine\\mongodb',
+                'server' => 'mongodb://localhost:27017',
+                'db' => 'kronos'
+            )
+        );
+
+        $request = new \iriki\request();
+        //db_type
+        $request->setDBType('\iriki\engine\mongodb');
+        //model status
+        $request->setModelStatus(
+            array(
+                'str' => 'test', //string, model
+                'str_full' => '\iriki\test', //string, full model including namespace
+                'defined' => true, //boolean, model defined in app or engine config
+                'exists' => true, //boolean, model class exists
+                'details' => array(
+                    "description" => "A test model.",
+                    "properties" => array(
+                        "_id" => array(
+                            "description" => "Internal unique ID.",
+                            "type" => "key",
+                            "unique" => true
+                        ),
+                        "property" => array(
+                            "description" => "Some other property.",
+                            "type" => "string"
+                        )
+                    ),
+                    "relationships" => array(
+                        "belongsto" => ["test2"],
+                        "hasmany" => []
+                    )
+                ), //array, model description, properties and relationships
+                'app_defined' => false, //boolean, model defined in app. otherwise engine
+                'action'=> 'action', //string, action
+                'default' => array(), //array, default actions
+                'action_defined' => true, //boolean, action defined
+                'action_default' => false, //boolean, action is default defined
+                'action_exists' => true, //boolean, action exists in class
+                'action_details' => array(
+                    "action" => array(
+                        "description" => "Test action",
+                        "parameters" => [],
+                        "exempt" => ['*'],
+                        "authenticate" => true
+                    )
+                ) //array, action description, parameters, exempt, authenticate
+            )
+        );
+        //parameter_status
+        $request->setParameterStatus(
+            array(
+                //properties supplied
+                'final' => array(),
+                //missing properties that should have been supplied
+                'missing' => array('property'),
+                //extra properties that should not have been supplied
+                'extra' => array(),
+                //these, especially for mongodb have to be saved as mongoids
+                'ids' => array()
+            )
+        );
+        //data
+        $request->setData(array());
+        //meta
+        //?
+        //session
+        $request->setSession('user_session_token');
+
+        $status = \iriki\engine\mongodb::doUpdate($request);
+
+        //assert
+        $this->assertEquals(true, isset($status['code']));
+    }
+
+    public function test_doDelete_error()
+    {
+        iriki\engine\mongodb::doInitialise(
+            array(
+                'type' => '\\iriki\\engine\\mongodb',
+                'server' => 'mongodb://localhost:27017',
+                'db' => 'kronos'
+            )
+        );
+
+        $request = new \iriki\request();
+        //db_type
+        $request->setDBType('\iriki\engine\mongodb');
+        //model status
+        $request->setModelStatus(
+            array(
+                'str' => 'test', //string, model
+                'str_full' => '\iriki\test', //string, full model including namespace
+                'defined' => true, //boolean, model defined in app or engine config
+                'exists' => true, //boolean, model class exists
+                'details' => array(
+                    "description" => "A test model.",
+                    "properties" => array(
+                        "_id" => array(
+                            "description" => "Internal unique ID.",
+                            "type" => "key",
+                            "unique" => true
+                        ),
+                        "property" => array(
+                            "description" => "Some other property.",
+                            "type" => "string"
+                        )
+                    ),
+                    "relationships" => array(
+                        "belongsto" => ["test2"],
+                        "hasmany" => []
+                    )
+                ), //array, model description, properties and relationships
+                'app_defined' => false, //boolean, model defined in app. otherwise engine
+                'action'=> 'action', //string, action
+                'default' => array(), //array, default actions
+                'action_defined' => true, //boolean, action defined
+                'action_default' => false, //boolean, action is default defined
+                'action_exists' => true, //boolean, action exists in class
+                'action_details' => array(
+                    "action" => array(
+                        "description" => "Test action",
+                        "parameters" => [],
+                        "exempt" => ['*'],
+                        "authenticate" => true
+                    )
+                ) //array, action description, parameters, exempt, authenticate
+            )
+        );
+        //parameter_status
+        $request->setParameterStatus(
+            array(
+                //properties supplied
+                'final' => array(),
+                //missing properties that should have been supplied
+                'missing' => array('property'),
+                //extra properties that should not have been supplied
+                'extra' => array(),
+                //these, especially for mongodb have to be saved as mongoids
+                'ids' => array()
+            )
+        );
+        //data
+        $request->setData(array());
+        //meta
+        //?
+        //session
+        $request->setSession('user_session_token');
+
+        $status = \iriki\engine\mongodb::doDelete($request);
+
+        //assert
+        $this->assertEquals(true, isset($status['code']));
+    }
 }
 
 ?>
--- engine/tests/database/mongodbsuccessTest.php
+++ engine/tests/database/mongodbsuccessTest.php
@@ -67,7 +67,7 @@ class mongodbsuccessTest extends \PHPUnit\Framework\TestCase
                         )
                     ),
                     "relationships" => array(
-                        "belongsto" => ["test2"],
+                        "belongsto" => [],
                         "hasmany" => []
                     )
                 ), //array, model description, properties and relationships
@@ -114,9 +114,14 @@ class mongodbsuccessTest extends \PHPUnit\Framework\TestCase
 
         //assert
         $this->assertEquals(true, $status['message']);
+
+        if (isset($status['data'])) return $status['data'];
     }
 
-    public function test_doRead_success()
+    /**
+     * @depends test_doCreate_success
+     */
+    public function test_doRead_success($id_to_read)
     {
         iriki\engine\mongodb::doDestroy();
     	
@@ -152,7 +157,7 @@ class mongodbsuccessTest extends \PHPUnit\Framework\TestCase
                         )
                     ),
                     "relationships" => array(
-                        "belongsto" => ["test2"],
+                        "belongsto" => [],
                         "hasmany" => []
                     )
                 ), //array, model description, properties and relationships
@@ -163,9 +168,9 @@ class mongodbsuccessTest extends \PHPUnit\Framework\TestCase
                 'action_default' => false, //boolean, action is default defined
                 'action_exists' => true, //boolean, action exists in class
                 'action_details' => array(
-                    "action" => array(
-                        "description" => "Test action",
-                        "parameters" => ["description"],
+                    "read" => array(
+                        "description" => "Test read action",
+                        "parameters" => ["_id"],
                         "authenticate" => false
                     )
                 ) //array, action description, parameters, exempt, authenticate
@@ -175,19 +180,112 @@ class mongodbsuccessTest extends \PHPUnit\Framework\TestCase
         $request->setParameterStatus(
             array(
                 //properties supplied
-                'final' => array('property'),
+                'final' => array('_id'),
                 //missing properties that should have been supplied
                 'missing' => array(),
                 //extra properties that should not have been supplied
                 'extra' => array(),
                 //these, especially for mongodb have to be saved as mongoids
-                'ids' => array()
+                'ids' => array('_id')
+            )
+        );
+        //data
+        $request->setData(
+        	//reads all
+            array(
+            	'_id' => $id_to_read
+            )
+        );
+
+        //meta
+        //?
+        //session
+        //$request->setSession('user_session_token');
+
+        $status = \iriki\engine\mongodb::doRead($request, array());
+
+        //assert
+        $this->assertEquals($id_to_read, $status[0]['_id']);
+
+        return $status[0];
+    }
+
+    /**
+     * @depends test_doRead_success
+     */
+    public function test_doUpdate_success($obj_to_update)
+    {
+        iriki\engine\mongodb::doDestroy();
+    	
+        iriki\engine\mongodb::doInitialise(
+            array(
+                'type' => '\iriki\engine\mongodb',
+                'server' => 'mongodb://localhost:27017',
+                'db' => 'kronos'
+            )
+        );
+
+        $request = new \iriki\request();
+        //db_type
+        $request->setDBType('\iriki\engine\mongodb');
+        //model status
+        $request->setModelStatus(
+            array(
+                'str' => 'test', //string, model
+                'str_full' => '\iriki\test', //string, full model including namespace
+                'defined' => true, //boolean, model defined in app or engine config
+                'exists' => true, //boolean, model class exists
+                'details' => array(
+                    "description" => "A test model.",
+                    "properties" => array(
+                        "_id" => array(
+                            "description" => "Internal unique ID.",
+                            "type" => "key",
+                            "unique" => true
+                        ),
+                        "property" => array(
+                            "description" => "Some other property.",
+                            "type" => "string"
+                        )
+                    ),
+                    "relationships" => array(
+                        "belongsto" => [],
+                        "hasmany" => []
+                    )
+                ), //array, model description, properties and relationships
+                'app_defined' => false, //boolean, model defined in app. otherwise engine
+                'action'=> 'action', //string, action
+                'default' => array(), //array, default actions
+                'action_defined' => true, //boolean, action defined
+                'action_default' => false, //boolean, action is default defined
+                'action_exists' => true, //boolean, action exists in class
+                'action_details' => array(
+                    "update" => array(
+                        "description" => "Test update action",
+                        "parameters" => [],
+                        "authenticate" => false
+                    )
+                ) //array, action description, parameters, exempt, authenticate
+            )
+        );
+        //parameter_status
+        $request->setParameterStatus(
+            array(
+                //properties supplied
+                'final' => array('_id', 'property'),
+                //missing properties that should have been supplied
+                'missing' => array(),
+                //extra properties that should not have been supplied
+                'extra' => array(),
+                //these, especially for mongodb have to be saved as mongoids
+                'ids' => array('_id')
             )
         );
         //data
         $request->setData(
             array(
-                'property' => "property's value"
+            	'_id' => $obj_to_update['_id'],
+            	'property' => 'changed property'
             )
         );
         //meta
@@ -195,10 +293,100 @@ class mongodbsuccessTest extends \PHPUnit\Framework\TestCase
         //session
         //$request->setSession('user_session_token');
 
-        $status = \iriki\engine\mongodb::doCreate($request);
+        $status = \iriki\engine\mongodb::doUpdate($request);
 
         //assert
-        $this->assertEquals(true, $status['message']);
+        $this->assertEquals(true, $status);
+    }
+
+    /**
+     * @depends test_doCreate_success
+     */
+    public function test_doDelete_success($id)
+    {
+        iriki\engine\mongodb::doDestroy();
+    	
+        iriki\engine\mongodb::doInitialise(
+            array(
+                'type' => '\iriki\engine\mongodb',
+                'server' => 'mongodb://localhost:27017',
+                'db' => 'kronos'
+            )
+        );
+
+        $request = new \iriki\request();
+        //db_type
+        $request->setDBType('\iriki\engine\mongodb');
+        //model status
+        $request->setModelStatus(
+            array(
+                'str' => 'test', //string, model
+                'str_full' => '\iriki\test', //string, full model including namespace
+                'defined' => true, //boolean, model defined in app or engine config
+                'exists' => true, //boolean, model class exists
+                'details' => array(
+                    "description" => "A test model.",
+                    "properties" => array(
+                        "_id" => array(
+                            "description" => "Internal unique ID.",
+                            "type" => "key",
+                            "unique" => true
+                        ),
+                        "property" => array(
+                            "description" => "Some other property.",
+                            "type" => "string"
+                        )
+                    ),
+                    "relationships" => array(
+                        "belongsto" => [],
+                        "hasmany" => []
+                    )
+                ), //array, model description, properties and relationships
+                'app_defined' => false, //boolean, model defined in app. otherwise engine
+                'action'=> 'action', //string, action
+                'default' => array(), //array, default actions
+                'action_defined' => true, //boolean, action defined
+                'action_default' => false, //boolean, action is default defined
+                'action_exists' => true, //boolean, action exists in class
+                'action_details' => array(
+                    "delete" => array(
+                        "description" => "Test delete action",
+                        "parameters" => ['_id'],
+                        "authenticate" => false
+                    )
+                ) //array, action description, parameters, exempt, authenticate
+            )
+        );
+        //parameter_status
+        $request->setParameterStatus(
+            array(
+                //properties supplied
+                'final' => array('_id'),
+                //missing properties that should have been supplied
+                'missing' => array(),
+                //extra properties that should not have been supplied
+                'extra' => array(),
+                //these, especially for mongodb have to be saved as mongoids
+                'ids' => array('_id')
+            )
+        );
+        //data
+        $request->setData(
+            array(
+            	'_id' => $id
+            )
+        );
+        //meta
+        //?
+        //session
+        //$request->setSession('user_session_token');
+
+        $status = \iriki\engine\mongodb::doDelete($request);
+
+        var_dump($status);
+
+        //assert
+        $this->assertEquals(true, $status);
     }
 }
 
