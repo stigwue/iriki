3dcea3b71c01a021e2006cd372d10cabb2d5ccfe
Stephen Igwue <olibenu@yahoo.com>
2018-08-09 17:29:57 +0100

    added user_group create_many and tests, fixed problem in model where authenticate for a default defined route was not correctly interpreted

--- engine/tests/userGroupTest.php
+++ engine/tests/userGroupTest.php
@@ -24,7 +24,7 @@ class userGroupTest extends \PHPUnit\Framework\TestCase
             'code' => 200,
             'message' => '',
             'data' => array(
-                'model' => 'group',
+                'model' => 'user_group',
                 'action' => 'create',
                 'url_parameters' => array(),
                 'params' => array(
@@ -50,15 +50,51 @@ class userGroupTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * @depends test_class_exist
+     */
+    public function test_create_many_success($status)
+    {
+        $groups = ['admin', 'regular'];
+
+        $request = array(
+            'code' => 200,
+            'message' => '',
+            'data' => array(
+                'model' => 'user_group',
+                'action' => 'create_many',
+                'url_parameters' => array(),
+                'params' => array(
+                    'title' => $groups
+                )
+            )
+        );
+
+        $model_profile = \iriki\engine\route::buildModelProfile($GLOBALS['APP'], $request);
+
+        //handle the request: match a route to a model and its action
+        $status = \iriki\engine\route::matchRequestToModel(
+            $GLOBALS['APP'],
+            $model_profile,
+            $request,
+            true //enable test mode
+        );
+
+        $this->assertEquals(true, 
+            ($status['code'] == 200) AND
+            (count($status['data']) == count($groups))
+        );
+    }
+
+    /**
 	 * @depends test_create_success
      */
-    public function test_read_by_title_success($title)
+    public function txst_read_by_title_success($title)
 	{
         $request = array(
             'code' => 200,
             'message' => '',
             'data' => array(
-                'model' => 'group',
+                'model' => 'user_group',
                 'action' => 'read_by_title',
                 'url_parameters' => array(),
                 'params' => array(
@@ -82,17 +118,15 @@ class userGroupTest extends \PHPUnit\Framework\TestCase
             count($status['data']) == 1) AND
             ($status['data'][0]['title'] == $title)
         );
-
 	}
 
-    /*
-    public function test_delete_success($id)
+     public function test_delete_success($id)
 	{
         $request = array(
             'code' => 200,
             'message' => '',
             'data' => array(
-                'model' => 'group',
+                'model' => 'user_group',
                 'action' => 'delete',
                 'url_parameters' => array(),
                 'params' => array(
@@ -116,7 +150,6 @@ class userGroupTest extends \PHPUnit\Framework\TestCase
             ($status['message'] == true))
         );
 	}
-    */
 }
 
 ?>
